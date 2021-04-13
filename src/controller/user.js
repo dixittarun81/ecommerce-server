0056400b6698de63f92f053e1b8ee021a60a77f4
tarun dixit <dixittarun81@gmail.com>
2021-04-13 12:29:45 +0530

    signin route created

--- src/controller/user.js
+++ src/controller/user.js
@@ -1,35 +1,63 @@
 const User = require("../models/user");
+const jwt = require("jsonwebtoken");
 
 exports.signup = (req, res) => {
-  User.findOne({ email: req.body.email }).exec((error, user) => {
-    if (user) {
-      return res.status(400).json({
-        message: "User already registered",
-      });
-    }
-
-    const { firstName, lastName, email, password } = req.body;
-
-    const _user = new User({
-      firstName,
-      lastName,
-      email,
-      password,
-      userName: Math.random().toString(),
-    });
-
-    _user.save((error, data) => {
-      if (error) {
+    User.findOne({ email: req.body.email }).exec((error, user) => {
+      if (user) {
         return res.status(400).json({
-          message: "Something went wrong",
+          message: "User already registered",
         });
       }
 
-      if (data) {
-        return res.status(201).json({
-          user: data,
-        });
-      }
+      const { firstName, lastName, email, password } = req.body;
+
+      const _user = new User({
+        firstName,
+        lastName,
+        email,
+        password,
+        userName: Math.random().toString(),
+      });
+
+      _user.save((error, data) => {
+        if (error) {
+          return res.status(400).json({
+            message: "Something went wrong",
+          });
+        }
+
+        if (data) {
+          return res.status(201).json({
+            message: "User registered successfully",
+          });
+        }
+      });
     });
-  });
+  };
+
+exports.signin = (req, res) => {
+    User.findOne({email: req.body.email}).exec((error,user) => {
+        if(error) return res.status(400).json({error});
+
+        if(user){
+
+            if(user.authenticate(req.body.password)){
+                const token = jwt.sign({_id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
+                const { _id, firstName, lastName, email, role, fullName} = user;
+                res.status(200).json({
+                    token,
+                    user: {
+                        _id, firstName, lastName, email, role, fullName
+                    }
+                })
+            }else {
+                return res.status(400).json({
+                    message : 'Invalid Password'
+                })
+            }
+
+        }else{
+            return res.status(400).json({message: 'Something went wrong'});
+        }
+    })
 };
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const User = require("../models/user");
-const {signup} = require('../controller/user');
+const {signup,signin} = require('../controller/user');
 
 //Express Router
 const router = express.Router();
 
-//sign in route
-router.post("/signin", (req, res) => {});
-
 //Sign up route
 router.post("/signup", signup);
+
+//sign in route
+router.post("/signin", signin);
+
+
+
+
 
 module.exports = router;

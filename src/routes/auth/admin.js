78af773545bb54be9e30d6f6297a3070a56a6e38
tarun dixit <dixittarun81@gmail.com>
2021-04-13 18:31:57 +0530

    validation middleware for signin and signup routes

--- src/routes/auth/admin.js
+++ src/routes/auth/admin.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const {signup,signin} = require('../../controller/auth/auth');
+const {validateSignupRequest,validateSigninRequest, isRequestValidated} = require('../../validators/auth');
 
 //Express Router
 const router = express.Router();
 
 //Sign up route
-router.post("/admin/signup", signup);
+router.post("/admin/signup",validateSignupRequest,isRequestValidated, signup);
 
 //sign in route
-router.post("/admin/signin", signin);
+router.post("/admin/signin",validateSigninRequest, isRequestValidated, signin);
 
 
 module.exports = router;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const User = require("../models/user");
 const {signup,signin,requireSignIn} = require('../controller/user');
+const {validateSignupRequest,validateSigninRequest, isRequestValidated} = require('../validators/auth');
 
 //Express Router
 const router = express.Router();
 
 //Sign up route
-router.post("/signup", signup);
+router.post("/signup",validateSignupRequest,isRequestValidated, signup);
 
 //sign in route
-router.post("/signin", signin);
+router.post("/signin",validateSigninRequest,isRequestValidated, signin);
 
 router.post("/profile", requireSignIn, (req,res) => {
     res.status(200).json({user : 'profile'});

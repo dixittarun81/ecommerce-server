55285c657c0e2db0beef254b2ddb6ea63441a4e5
tarun dixit <dixittarun81@gmail.com>
2021-04-20 19:57:33 +0530

    InitialData Api Created

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ const adminRoutes = require("./routes/auth/admin");
 const categoryRoutes = require('./routes/category');
 const productRoutes = require('./routes/product');
 const cartRoutes = require('./routes/cart');
+const initialDataRoutes = require('./routes/auth/initialData');
 
 //Initiating express app
 const app = express();
@@ -37,6 +38,7 @@ app.use('/api',adminRoutes);
 app.use('/api',categoryRoutes);
 app.use('/api',productRoutes);
 app.use('/api',cartRoutes);
+app.use('/api', initialDataRoutes);
 
 
 
---
+++ src/routes/auth/initialData.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {initialData} = require('../../controller/auth/initialData');
+
+router.post("/initialdata",initialData);
+
+module.exports = router;
+
+
